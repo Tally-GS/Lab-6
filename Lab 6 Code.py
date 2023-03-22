a677947fbee26e17c1d2d45d0ebd4eb562c339da
Tally-GS <natalie.smith@ufl.edu>
2023-03-22 13:56:44 -0400

    Made File

--- Lab 6 Code.py
+++ Lab 6 Code.py
@@ -1,1 +1,34 @@
-#no code yet
+
+def menu(): #menu function
+    print("\nMenu\n------------\n1. Encode\n2. Decode\n3. Quit\n")
+
+def encode(password): #pass encoder
+    encoded = ""
+    for i in password:
+        encoded = encoded + str((int(i) + 3) % 10) #shifts number and makes sure each number stays single diget
+    return encoded
+
+def decode(password): #pass decoder
+    decoded = ""
+    for i in password:
+        decoded = decoded + str((int(i) - 3) % 10) #shifts number and makes sure each number stays single diget
+    return decoded
+def main():
+    decoder = True
+    while decoder:
+        menu()
+        option = int(input("Please enter an option: ")) #makes it integer so if statement can run
+        if option == 1:
+            user_password = input("Please enter your password to encode: ")
+            encoded_password = encode(user_password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        elif option == 3:
+            decoder = False
+            break
+
+
+if __name__ == '__main__':
+    main()
